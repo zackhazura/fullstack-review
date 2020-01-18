3bdacd7ca699c0a729465c351ccab703424b1c50
Zack Singh <zacharysingh@gmail.com>
2020-01-18 09:55:23 -0800

    Change database insert method to save()

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
-  git_id: {type: Number, unique: true,},
+  git_id: { type: Number, unique: true },
   owner: String,
   git_name: String,
   url: String,
@@ -12,6 +12,7 @@ let repoSchema = mongoose.Schema({
   forks: Number
 });
 
+
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (gitData) => {
@@ -19,15 +20,17 @@ let save = (gitData) => {
   // This function should save a repo or repos to
   // the MongoDB
 
-  Repo.insertMany([
-    {git_id: gitData.id},
-    {owner: gitData.owner.login},
-    {git_name: gitData.name},
-    {url: gitData.html_url},
-    {desc: gitData.description},
-    {stargazers: gitData.stargazers_count},
-    {forks: gitData.forks_count}
-  ])
+  var repoData = new Repo({
+    git_id: gitData.id,
+    owner: gitData.owner.login,
+    git_name: gitData.name,
+    url: gitData.html_url,
+    desc: gitData.description,
+    stargazers: gitData.stargazers_count,
+    forks: gitData.forks_count
+  });
+
+  repoData.save();
 }
 
 module.exports.save = save;
